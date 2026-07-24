88fe57e1efda4fc005cf33e6afb4ed9a4c3dc853
TaiNguyenNTT <tainguyenntt00@gmail.com>
2026-07-24 14:42:27 +0700

    add do while to class loop practice

--- src/Loop_Practice.java
+++ src/Loop_Practice.java
@@ -18,12 +18,25 @@ public class Loop_Practice {
     System.out.println("================================================");
     int i = 1;
     total = 0;
+    inputNumber = scanner.nextInt();
     while (i <= inputNumber) {
       System.out.print("Loop while " + i + ": " + i + " + " + total + " = ");
       total = total + i;
       System.out.println(total);
       i++;
     }
-  }
 
+    // do while
+    System.out.println("================================================");
+    total = 0;
+    i = 1;
+    inputNumber = scanner.nextInt();
+    do{
+      System.out.print("Loop do while " + i + ": " + i + " + " + total + " = ");
+      total = total + i;
+      System.out.println(total);
+      i++;
+    }
+    while (i <= inputNumber);
+  }
 }
